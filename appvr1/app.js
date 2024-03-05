38db746f33c0cf5da80b457b7f61abcdf8caf852
AaronVelasco93 <aaronvelasco1933@outlook.com>
2024-03-05 10:49:33 -0600

    Creacion de funcion

--- appvr1/app.js
+++ appvr1/app.js
@@ -1,4 +1,14 @@
 function saludar(){
     alert("Bienvenidos!!!");
 }
-saludar();
+
+function suma(n1, n2) {
+    return parseInt(n1) + parseInt(n2); 
+}
+
+let numero1 = prompt("Ingresa el numero 1: ");
+let numero2 = prompt("Ingresa el numero 2: ");
+let resultado = suma(numero1, numero2);
+
+console.log(resultado);
+//saludar();
